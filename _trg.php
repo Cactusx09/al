f94a62173baf42562b2ef048f88fe7f70c2e09db
cactusx09 <cactusx09@gmail.com>
2018-03-30 17:26:09 +0300

    remove contacts logo animation

--- _trg.php
+++ _trg.php
@@ -16,3 +16,57 @@
     }
     echo $link;
 ?>
+<?php
+    if(is_dir('pages')){
+        $dir = opendir(pages);
+        $href = 'pages';
+    } else{
+        $dir = opendir(__DIR__);
+        $href = '';
+    }
+    $i = 0;
+    $link = '';
+    while (false !== ($file = readdir($dir))) {
+        $i++;
+        if (strpos($file, '.html',1) ) {
+            $link .= '<a href="'.$href.'/'.$file.'">'.$file.'</a>';
+        }
+    }
+    echo $link;
+?>
+<?php
+    if(is_dir('pages')){
+        $dir = opendir(pages);
+        $href = 'pages';
+    } else{
+        $dir = opendir(__DIR__);
+        $href = '';
+    }
+    $i = 0;
+    $link = '';
+    while (false !== ($file = readdir($dir))) {
+        $i++;
+        if (strpos($file, '.html',1) ) {
+            $link .= '<a href="'.$href.'/'.$file.'">'.$file.'</a>';
+        }
+    }
+    echo $link;
+?>
+<?php
+    if(is_dir('pages')){
+        $dir = opendir(pages);
+        $href = 'pages';
+    } else{
+        $dir = opendir(__DIR__);
+        $href = '';
+    }
+    $i = 0;
+    $link = '';
+    while (false !== ($file = readdir($dir))) {
+        $i++;
+        if (strpos($file, '.html',1) ) {
+            $link .= '<a href="'.$href.'/'.$file.'">'.$file.'</a>';
+        }
+    }
+    echo $link;
+?>
